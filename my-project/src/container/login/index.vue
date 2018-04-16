11edce165ca2230b259a778ee26152013e2f2bf2
zhangshuo <zhangshuo@lvwan.com>
2018-04-16 15:41:53 +0800

    master: change some config

--- my-project/src/container/login/index.vue
+++ my-project/src/container/login/index.vue
@@ -11,7 +11,7 @@
 
 <script>
   export default {
-    name: 'hello',
+    name: 'login',
     data () {
       return {
         username: '',
--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -23,7 +23,7 @@ export default new Router({
     },
     {
       path: '*',
-      name: 'login',
+      name: 'default',
       component: require('../container/login/index.vue')
     }
   ]
